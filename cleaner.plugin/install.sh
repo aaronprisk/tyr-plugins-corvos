f244980edc554a0ce26466a661ee37ba06410bd1
Aaron Prisk <aaronprisk@gmail.com>
2018-06-27 18:56:08 -0400

    Add Cleaner Plugin

---
+++ cleaner.plugin/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo cp /opt/corvos/student-profile/Cleaner.desktop /home/student/.config/autostart/
---
+++ cleaner.plugin/uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm /home/student/.config/autostart/Cleaner.desktop
